f45e37e0452d71cc730be3251bf45ecc4602de5c
Ayaka2412 <tl211332@cis.fukuoka-u.ac.jp>
2024-07-14 15:07:17 +0900

    通知画面の実装

--- calorie_go_flutter/lib/pages/notice/notice_page.dart
+++ calorie_go_flutter/lib/pages/notice/notice_page.dart
@@ -1,23 +1,54 @@
 import 'package:calorie_go_flutter/components/bottom_app_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 class NoticePage extends StatelessWidget {
   const NoticePage({super.key});
-  // final int index;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Notice Page'),
+        title: const Text('お知らせ'),
+        elevation: 10,
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            context.push('/sample');
-          },
-          child: const Text('Go to Sample Page'),
-        ),
+      body: ListView.builder(
+        itemCount: 20,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text('Item $index'),
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text('Item $index'),
+                    content: Text('お知らせ $index の詳細欄'),
+                    actions: <Widget>[
+                      Center(
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          style: ButtonStyle(
+                            foregroundColor: WidgetStateProperty.all(
+                                Colors.white),
+                            backgroundColor: WidgetStateProperty.all(
+                                Colors.lightBlue),
+                          ),
+                          child: const Text('OK'),
+                        ),
+                      ),
+                    ],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      side: const BorderSide(color: Colors.lightBlue, width: 2),
+                    ),
+                  );
+                },
+              );
+            },
+          );
+        },
       ),
       bottomNavigationBar: const AppBottomNavigationBar(),
     );
