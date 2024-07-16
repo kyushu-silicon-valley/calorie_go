c6a079f72784dfce4cc785d66d4322a43db8ab8d
Ayaka2412 <tl211332@cis.fukuoka-u.ac.jp>
2024-07-16 17:28:02 +0900

    ホームページのバックエンド繋ぎ込み

---
+++ calorie_go_flutter/lib/pages/home/home_page_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'home_page_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$homePageControllerHash() =>
+    r'7787a70eaaafe53eda5361b8cb8c582378e1f76e';
+
+/// See also [HomePageController].
+@ProviderFor(HomePageController)
+final homePageControllerProvider =
+    AutoDisposeNotifierProvider<HomePageController, HomePageState>.internal(
+  HomePageController.new,
+  name: r'homePageControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$homePageControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$HomePageController = AutoDisposeNotifier<HomePageState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
