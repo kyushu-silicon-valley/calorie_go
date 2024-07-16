c032f4f8cc60f219bb37e002596dda4603c7d54b
urata <take4dolphin@gmail.com>
2024-07-16 16:25:01 +0900

    設定ページの見た目作りました

--- calorie_go_flutter/lib/pages/setting/setting_page.dart
+++ calorie_go_flutter/lib/pages/setting/setting_page.dart
@@ -1,33 +1,245 @@
 import 'package:calorie_go_flutter/components/bottom_app_bar.dart';
 import 'package:calorie_go_flutter/constants.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+  enum GenderValue { man, woman}
+
 class SettingPage extends HookConsumerWidget {
   const SettingPage({super.key});
+
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Setting'),
+        elevation: 10,
+        title: const Text('設定'),
       ),
       body: Center(
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () async {
-                await sessionManager.signOut();
-                if (!context.mounted) return;
-                context.go('/top');
-              },
-              child: const Text('LogOut'),
-            ),
-          ],
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width * 4/5,
+          child: ListView(
+            children: [
+              const Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  'プロフィール情報',
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 4/5,
+                  child: Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 226, 226, 226),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        child: const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  'ニックネーム',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ),
+                              TextField(
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                                  ),
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  '性別',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Radio(
+                                      value: GenderValue.man,
+                                      groupValue: GenderValue.man,
+                                      onChanged: null,
+                                    ),
+                                    Text('男性'),
+                                    ]   
+                                  ),
+                                  Row(
+                                    children: [
+                                      Radio(
+                                      value: GenderValue.man,
+                                      groupValue: GenderValue.woman,
+                                      onChanged: null,
+                                    ),
+                                    Text('女性'),
+                                    ]   
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: const ElevatedButton(
+                            onPressed: null,
+                            child: Text(
+                              '登録',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'アプリ設定',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 226, 226, 226),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'ダークモード',
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                                CupertinoSwitch(
+                                  value: false,
+                                  onChanged: null,
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 226, 226, 226),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Column(
+                              children: [
+                                Align(
+                                  alignment: Alignment.topLeft,
+                                  child: Text(
+                                    'シリアルコード',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ),
+                                TextField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Text(
+                                      '問い合わせ',
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Text(
+                                      'キャッシュ削除',
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Text(
+                                      'ログアウトする',
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Text(
+                                      '退会する',
+                                      style: TextStyle(
+                                        color: Colors.red,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
-      bottomNavigationBar: const AppBottomNavigationBar(),
     );
   }
 }
